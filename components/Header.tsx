9aedfd99d53e4539035d314b174b27acc2cd1f8d
SaadShakib <imsaadshakib@gmail.com>
2021-04-03 09:56:50 +0600

    contact for validation

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ function Header():JSX.Element {
         <div  className="h-16 flex items-center shadow-lg justify-around">
             <div className="flex items-center">
             <img className="h-10 w-10 border-2 border-green-500 rounded-full" src="/images/shadman.jfif"  alt="Shadman Shakib"/>
-          <h1 className="ml-3 font-bold text-green-500"><Link  href="/">SHADMAN SHAKIB</Link></h1> 
+          <h1 className="ml-3 font-bold text-xl text-green-500"><Link  href="/">SHADMAN SHAKIB</Link></h1> 
             </div>
                <ul className="hidden xl:flex justify-around w-96">
                    <li>HOME</li>
