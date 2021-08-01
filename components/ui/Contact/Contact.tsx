56a4d2891c3c6090f18d329e731f13038f9e440a
Shadman Shakib <sm.shakib007@gmail.com>
2021-08-01 22:27:55 +0600

    contact

--- components/ui/Contact/Contact.tsx
+++ components/ui/Contact/Contact.tsx
@@ -88,7 +88,7 @@ function Contactme(): JSX.Element {
               <ErrorMessage name="message" />
             </div>
             <button
-              className="bg-green-500 hover:bg-green-700 font-semibold focus:outline-none text-white rounded-sm mt-10 px-3 py-2"
+              className="bg-primary  font-semibold focus:outline-none text-white rounded-sm mt-10 px-3 py-2"
               type="submit"
             >
               MESSAGE
