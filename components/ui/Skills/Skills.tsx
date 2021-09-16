ea24264e8ed7bdc952e4a14a846ca5f3946fb1c5
Shadman Shakib <sm.shakib007@gmail.com>
2021-09-16 09:38:15 +0600

    constants

--- components/ui/Skills/Skills.tsx
+++ components/ui/Skills/Skills.tsx
@@ -1,23 +1,10 @@
 import React from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import { SkillsData } from "@constants/index";
 interface SkillCard {
   name: string;
   src: string;
 }
-const SkillsData = [
-  { id: "adhfoafodaf", name: "React", img: "/images/react.png" },
-  { id: "scohbounalgv", name: "Redux", img: "/images/redux.svg" },
-  { id: "lhghhboeigg", name: "Next JS", img: "/images/next-js.svg" },
-  { id: "ljvoufhhhb", name: "Graphql", img: "/images/graphql.svg" },
-  { id: "aoeivnaoinc", name: "Node JS", img: "/images/nodejs.svg" },
-  { id: "oeihghhoeooeov", name: "Git", img: "/images/Giticon.png" },
-  {
-    id: "fhoabhhbhbhaloicice",
-    name: "Tailwind CSS",
-    img: "/images/tailwindcss.svg",
-  },
-  { id: "fjdjfdjvhutguxpe", name: "Ant Design", img: "/images/ant-design.svg" },
-];
+
 const Card = ({ name, src }: SkillCard) => {
   return (
     <div className="w-44 rounded-md justify-start pl-4 shadow-md flex items-center h-16 bg-white ">
---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export { SkillsData } from "./Data";
