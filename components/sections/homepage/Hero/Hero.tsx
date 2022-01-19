613e4e5448d9659b87a8a1558aebfbdf59f256ae
Shadman Shakib <sm.shakib007@gmail.com>
2022-01-19 16:23:10 +0600

    Fetching data

--- components/sections/homepage/Hero/Hero.tsx
+++ components/sections/homepage/Hero/Hero.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import s from "./Hero.module.css";
-function Hero() {
+interface Props {
+  src: string;
+}
+function Hero(props: Props) {
+  const skills = ["React", "Next JS", "App"];
+  const [state, setState] = React.useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setState((state) => (state === skills.length - 1 ? 0 : state + 1));
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [skills.length]);
   return (
-    <div>
+    <section>
       <Image
         className={s.profile_image}
         alt="Shadman Shakib"
-        src="https://avatars.githubusercontent.com/u/72305492?v=4"
+        src={props.src}
         width={400}
         height={400}
       />
-    </div>
+      <h1 className="text-white text-3xl">
+        Hi, Im <span>Shadman Shakib </span>
+        <span>{skills[state]} </span>
+        developer
+      </h1>
+    </section>
   );
 }
 
