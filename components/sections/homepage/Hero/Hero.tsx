3836d87413963f42b4cd799dd002e75ced62674d
Shadman Shakib <sm.shakib007@gmail.com>
2022-01-19 19:52:12 +0600

    Shine

--- components/sections/homepage/Hero/Hero.tsx
+++ components/sections/homepage/Hero/Hero.tsx
@@ -5,26 +5,27 @@ interface Props {
   src: string;
 }
 function Hero(props: Props) {
-  const skills = ["React", "Next JS", "App"];
+  const skills = ["React", "Next JS", "React Native", "Shopify", "TypeScript"];
   const [state, setState] = React.useState(0);
   React.useEffect(() => {
     const interval = setInterval(() => {
       setState((state) => (state === skills.length - 1 ? 0 : state + 1));
-    }, 2000);
+    }, 3000);
     return () => clearInterval(interval);
   }, [skills.length]);
   return (
-    <section>
+    <section className={s.main}>
       <Image
         className={s.profile_image}
         alt="Shadman Shakib"
         src={props.src}
-        width={400}
-        height={400}
+        width={300}
+        height={300}
       />
       <h1 className="text-white text-3xl">
         Hi, Im <span>Shadman Shakib </span>
-        <span>{skills[state]} </span>
+        <br />
+        <span className={s.shine}>{skills[state]} </span>
         developer
       </h1>
     </section>
