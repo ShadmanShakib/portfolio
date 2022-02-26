afdb4f28246d36e0f808345d65a9c19c5a3d2cc9
Shadman Shakib <sm.shakib007@gmail.com>
2022-02-26 09:19:10 +0600

    icons

--- components/sections/common/footer/Footer.tsx
+++ components/sections/common/footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { BsTwitter } from "react-icons/bs";
+import { BsTwitter, BsGithub } from "react-icons/bs";
 import s from "./Footer.module.css";
 function Footer() {
   return (
     <div className={s.main}>
       <BsTwitter className={s.icon} size={30} />
+      <BsGithub className={s.icon} size={30} />
     </div>
   );
 }
