a6918dddb5bf574c6735faa691dde9079c4e2855
Shadman Shakib <sm.shakib007@gmail.com>
2022-05-07 13:57:41 +0600

    Skills

---
+++ components/sections/homepage/Skills/Skills.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <div>
+      Skills
+      <div className="">
+        <h1>Frontend</h1>
+        <h1>Typescript</h1>
+        <h1>React</h1>
+        <h1>Next.js</h1>
+      </div>
+      <div className="">
+        <h1>Backend</h1>
+        <h1>Node.js</h1>
+        <h1>Express</h1>
+        <h1>MongoDB</h1>
+        <h1>NestJs</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
