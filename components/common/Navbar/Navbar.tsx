9396946c83ba0a968830364d30d482dbf9455f29
Shadman Shakib <sm.shakib007@gmail.com>
2022-07-14 18:07:55 +0600

    Button Ui

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import { BsList } from "react-icons/bs";
+import { Button } from "components/ui";
 function Navbar() {
   return (
-    <div className="">
+    <nav className="flex h-16 items-center justify-between px-6 text-white">
+      <div>
+        <BsList size={30} color="white" />
+      </div>
       <Link href="/">
         <a>Shadman Shakib</a>
       </Link>
-      <div>
+
+      <div className="hidden">
         <Link href="/about">
           <a>About</a>
         </Link>
@@ -14,7 +20,12 @@ function Navbar() {
           <a>Projects</a>
         </Link>
       </div>
-    </div>
+      <Button variant="primary">
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+      </Button>
+    </nav>
   );
 }
 
---
+++ components/ui/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ButtonProps } from "./Button.types";
+import cn from "classnames";
+function Button(props: ButtonProps) {
+  const { children, variant } = props;
+  const classes = cn(
+    "bg-gray-900 text-white rounded-sm p-2",
+    variant === "primary" && "bg-blue-500",
+    variant === "secondary" && "bg-gray-900",
+    variant === "tertiary" && "bg-gray-700"
+  );
+  return <button className={classes}>{children}</button>;
+}
+
+export default Button;
