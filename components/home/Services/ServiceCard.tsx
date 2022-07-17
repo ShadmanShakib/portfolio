52c0a867e84ed1ea6427cbf846fd68a8cd3773b9
Shadman Shakib <sm.shakib007@gmail.com>
2022-07-17 22:42:37 +0600

    icon added

--- components/home/Services/ServiceCard.tsx
+++ components/home/Services/ServiceCard.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { ServiceCardProps } from "./Services.types";
-import { IoCodeSlash } from "react-icons/io5";
+import {
+  IoCodeSlash,
+  IoLogoAppleAppstore,
+  IoCloudCircleOutline,
+} from "react-icons/io5";
 function ServiceCard(props: ServiceCardProps) {
   const {
     service: { title, description, id, varient },
   } = props;
+  const icon =
+    varient === "web" ? (
+      <IoCodeSlash size={30} />
+    ) : varient === "mobile" ? (
+      <IoLogoAppleAppstore size={30} />
+    ) : varient === "api" ? (
+      <IoCloudCircleOutline size={30} />
+    ) : null;
   return (
     <article
-      className="flex flex-col items-center justify-center rounded-sm bg-white p-4 shadow-lg"
+      className="flex flex-col  rounded-sm bg-white p-4 shadow-lg"
       key={id}
     >
+      <div>{icon}</div>
+
       <h2 className="text-xl font-semibold">{title}</h2>
       <p>{description}</p>
     </article>
