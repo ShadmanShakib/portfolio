d6953a6f6bfbc02e02eeb917ee5eb22fb7e6f957
Shadman Shakib <sm.shakib007@gmail.com>
2022-07-17 12:49:48 +0600

    posts

--- components/home/Posts/Posts.tsx
+++ components/home/Posts/Posts.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { getPosts } from "services/hashnode";
+import _ from "underscore";
+import PostCard from "./PostCard";
 function Blog() {
+  const [posts, setPosts] = React.useState<any>([]);
   const getData = async () => {
     getPosts().then((res) => {
-      console.log(res);
+      const posts = res.data.user.publication.posts;
+      setPosts(posts);
+      console.log(posts);
     });
   };
+  React.useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <h1 className="text-3xl text-white">Posts</h1>
-      <button className="bg-white p-3" onClick={getData}>
-        Get
-      </button>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
+        {posts &&
+          _.map(posts, (post) => {
+            return (
+              <div key={post._id}>
+                <PostCard post={post} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["cdn.hashnode.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
