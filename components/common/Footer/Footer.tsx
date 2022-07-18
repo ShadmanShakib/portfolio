bc9db428559bf44a891f4c1af80442aca6f614e3
Shadman Shakib <sm.shakib007@gmail.com>
2022-07-18 23:15:52 +0600

    footer updated

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { IoLogoTwitter } from "react-icons/io5";
+import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 function Footer() {
   return (
     <footer>
       <div>
-        <h1>Shadman Shakib</h1>
-        <p>
+        <h1 className="text-white">Shadman Shakib</h1>
+        <p className="text-white">
           I build thing for web which your user will love and enjoy using
           everyday. I will work with you from start to finish so that you can
           reach your goal.
         </p>
+        <div className="flex">
+          <a href="https://twitter.com/shadmanshakib_">
+            <IoLogoTwitter size={30} color="white" />
+          </a>
+          <a href="https://github.com/shadmanshakib">
+            <IoLogoGithub size={30} color="white" />
+          </a>
+          <a href="https://www.linkedin.com/in/shadmanshakib007/">
+            <IoLogoLinkedin size={30} color="white" />
+          </a>
+        </div>
       </div>
-      <a href="https://twitter.com/shadmanshakib_">
-        <IoLogoTwitter size={40} color="gray" />
-      </a>
     </footer>
   );
 }
