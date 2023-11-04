3c4ea005bf4444148f8b21252af661771d4c55b2
ShadmanShakib <sm.shakib007@gmail.com>
2023-11-04 22:10:41 +0600

    hero

---
+++ app/(home)/hero.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+function Hero() {
+  return (
+    <section>
+      <div className="text-center">
+        <h1 className="text-3xl lg:text-8xl">
+          You&#39;ve got a idea,
+          <br />
+          let&apos;s make it a Reality
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
