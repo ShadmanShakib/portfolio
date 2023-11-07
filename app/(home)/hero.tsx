e29870e36d7734e87da861200382d0388ca164d5
ShadmanShakib <sm.shakib007@gmail.com>
2023-11-07 17:56:32 +0600

    Hero

--- app/(home)/hero.tsx
+++ app/(home)/hero.tsx
@@ -1,15 +1,23 @@
-
 import React from "react";
-
+import { Button } from "@/app/components/ui";
 function Hero() {
   return (
     <section>
       <div className="text-center">
-        <h1 className="text-3xl lg:text-8xl">
-          You&#39;ve got a idea,
-          <br />
-          let&apos;s make it a Reality
-        </h1>
+        <div className="">
+          <h1 className="mb-10 text-3xl lg:text-8xl">
+            Ship your idea,
+            <br />
+            start reciving payment
+          </h1>
+          <p className="text-xl">
+            All the technical support you need to turn your idea into a SaaS
+            business and sarting earning money
+          </p>
+        </div>
+        <div className="mt-10">
+          <Button className="w-36 font-semibold uppercase">Book a Call</Button>
+        </div>
       </div>
     </section>
   );
---
+++ app/(home)/services/Services.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Props = {};
+
+const Services = (props: Props) => {
+  return (
+    <section>
+      <article>
+        <div className="flex">
+          <div className="">
+            <h1>Admin Panel Development</h1>
+            <p>SaaS admin panel </p>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+};
+
+export default Services;
