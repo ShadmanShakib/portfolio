2cfbbd75c9a7a78ac6d81714ef9bc437e51f9dca
ShadmanShakib <sm.shakib007@gmail.com>
2023-11-27 21:57:10 +0600

    Header update

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Header } from "@components/common";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const layout = (props: Props) => {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <Header />
+      {props.children}
+    </div>
+  );
 };
 
 export default layout;
--- app/components/common/header/Header.tsx
+++ app/components/common/header/Header.tsx
@@ -3,7 +3,17 @@ import React from "react";
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-primary-background text-white flex h-16 items-center justify-between border-b border-[#2e2e36] py-3 xl:px-6">
+      <h1 className="text-primary-foreground ">Sadman Shakib</h1>
+      <div className="flex items-center space-x-4">
+        <div className="">Projects</div>
+        <div className="w-36 rounded-3xl bg-[#1c1c1e] py-2 text-center">
+          <p>Book a call</p>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
